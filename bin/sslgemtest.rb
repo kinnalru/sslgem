76a10efa215dfaedae2cbd5259187564b2eb6aaf
Samoilenko Yuri <kinnalru@gmail.com>
2014-01-23 13:07:54 +0400

    many fixes

--- bin/sslgemtest.rb
+++ bin/sslgemtest.rb
@@ -17,7 +17,7 @@ require 'sslgem'
 puts ">> Loading gem...    OK"
 
 puts "\n>> Creating ssl instance..."
-ssl = SslGem.new
+ssl = Ssl::SslGem.new
 puts ">> Creating ssl instance... OK"
 
 
@@ -36,24 +36,24 @@ begin
     
 
     puts "\n  >> Signing data 1..."
-    sign = ssl.sign(SslGem::TESTKEY, "11111111") 
+    sign = ssl.sign(Ssl::SslGem::TESTKEY, "11111111") 
     puts "  >> Result OK #{sign}"
     
     puts "\n  >> Signing data 2..."
-    sign = ssl.sign(SslGem::TESTKEY, "22222222")
+    sign = ssl.sign(Ssl::SslGem::TESTKEY, "22222222")
     puts "  >> Result OK #{sign}"
     
     begin
         puts "\n  >> Signing data with invalid key..."
         sign = ssl.sign("/not/existent/key", "11111111")
         raise "it MUST be impossible to sign data"
-    rescue SslGem::Error => e
+    rescue Ssl::SslGem::Error => e
         puts "  >> Result OK"
     end
     
 
     puts "\n  >> Signing file..."
-    sig = ssl.sign_file(SslGem::TESTKEY, SslGem::TESTCERT, __FILE__) 
+    sig = ssl.sign_file(Ssl::SslGem::TESTKEY, Ssl::SslGem::TESTCERT, __FILE__) 
     File.write "/tmp/temp.sig", sig
     puts "  >> Result OK"
     
@@ -65,7 +65,7 @@ begin
         puts "\n  >> Verify invalid signature file..."
         ssl.verify_file(__FILE__, __FILE__) 
         raise "it must be impossible to verify this file"
-    rescue SslGem::Error => e
+    rescue Ssl::SslGem::Error => e
         puts "  >> Result OK"
     end
 
@@ -74,13 +74,14 @@ begin
         File.write "/tmp/temp.test", "hello world"
         ssl.verify_file("/tmp/temp.sig", "/tmp/temp.test") 
         raise "it must be impossible to verify this file"
-    rescue SslGem::Error => e
+    rescue Ssl::SslGem::Error => e
         puts "  >> Result OK"
     end
     
     
     puts "\n  >> Extracting certificates..."
-    certs = ssl.extract_certs File.read(SslGem::TESTRESPONSE)
+    certs = ssl.extract_certs File.read(Ssl::SslGem::TESTRESPONSE)
+	puts certs
     raise "Expected [1] certs occured [#{certs.size}]" if certs.size != 1
     raise "Not Before invalid" if certs.first.not_before.to_s != "2013-03-28 11:51:00 UTC"
     raise "Not After invalid" if certs.first.not_after.to_s !="2014-03-14 11:56:00 UTC"
--- lib/sslgem.rb
+++ lib/sslgem.rb
@@ -6,115 +6,153 @@ require 'fiddle'
 
 require 'openssl'
 
-#patching Certificate class
-OpenSSL::X509::Certificate.class_eval "
+module Ssl
 
-    alias :old_initialize :initialize
+  class SslGem
 
-    def repack_name name, type
-        return name.sub(\"#\{type\}:\", '').strip.gsub(\" = \", \"=\").gsub(\", \", \"/\")
+    IMAGEPATH = File.dirname(__FILE__) + "/../ext/ssl/image/"
+    ENV['PATH']="#{IMAGEPATH}/bin:#{ENV['PATH']}"
+    SSLLIB = Fiddle::Handle.new(IMAGEPATH + "/lib/libopenssl.so", Fiddle::RTLD_LAZY | Fiddle::RTLD_GLOBAL)
+
+    TESTKEY = File.dirname(__FILE__) + "/test/seckey.pem"
+    TESTCERT = File.dirname(__FILE__) + "/test/cert.pem"
+    TESTRESPONSE = File.dirname(__FILE__) + "/test/response"
+
+    class Error < RuntimeError
+      def initialize(message)
+        super(message)
+      end
     end
-
-    def initialize args = nil
-        old_initialize(args)
-        
-        begin
-            stdout, stderr, status = Open3.capture3('openssl x509 -noout -text -certopt no_pubkey -certopt no_sigdump -nameopt oneline,-esc_msb', stdin_data: self.to_s, binmode: true)
-        rescue
-            return
-        end
-        
-        begin
-            self.issuer = OpenSSL::X509::Name.parse repack_name(stdout.split('\n').grep(/Issuer/).first, 'Issuer')
-        rescue
-        end
-        
-        begin
-            self.subject = OpenSSL::X509::Name.parse repack_name(stdout.split('\n').grep(/Subject/).first, 'Subject')
-        rescue
-        end
-    end
-"
-
-class SslGem
-
-  IMAGEPATH = File.dirname(__FILE__) + "/../ext/ssl/image/"
-  ENV['PATH']="#{IMAGEPATH}/bin:#{ENV['PATH']}"
-  SSLLIB = Fiddle::Handle.new(IMAGEPATH + "/lib/libopenssl.so", Fiddle::RTLD_LAZY | Fiddle::RTLD_GLOBAL)
-
-  TESTKEY = File.dirname(__FILE__) + "/test/seckey.pem"
-  TESTCERT = File.dirname(__FILE__) + "/test/cert.pem"
-  TESTRESPONSE = File.dirname(__FILE__) + "/test/response"
-
-  class Error < RuntimeError
-    def initialize(message)
-      super(message)
-    end
-  end
-  
-  def info
-    return "SslGem info:\n
-      using openssl: #{`which openssl`}\n
-      version: #{`openssl version -a`}
-      engines: #{`openssl engine -t`}
-    "
-  end
-
-  def dgst data
-    stdout, stderr, status = Open3.capture3('openssl dgst -engine gost -md_gost94 -binary', stdin_data: data, binmode: true)
-
-    if status.success?
-      return (Base64.encode64 stdout).strip
-    else
-      raise Error.new("dgst failed: #{stderr}")
-    end
-  end
     
-  def sign key, data
-    stdout, stderr, status = Open3.capture3("openssl dgst -engine gost -sign #{key}", stdin_data: data, binmode: true)
-
-    if status.success?
-      return (Base64.encode64 stdout).strip
-    else
-      raise Error.new("sign failed: #{stderr}")
+    def info
+      return "SslGem info:\n
+        using openssl: #{`which openssl`}\n
+        version: #{`openssl version -a`}
+        engines: #{`openssl engine -t`}
+      "
     end
-  end
 
+    def dgst data
+      stdout, stderr, status = Open3.capture3('openssl dgst -engine gost -md_gost94 -binary', stdin_data: data, binmode: true)
 
-  def verify_file signature, file
-    stdout, stderr, status = Open3.capture3("openssl smime -verify -engine gost -noverify -inform DER -in #{signature} -content #{file}", binmode: true)
-
-    if status.success?
-      return /successful/ =~ stdout
-    else
-      raise Error.new("verify_file failed: #{stderr}")
+      if status.success?
+        return (Base64.encode64 stdout).strip
+      else
+        raise Error.new("dgst failed: #{stderr}")
+      end
     end
-  end
+      
+    def sign key, data
+      stdout, stderr, status = Open3.capture3("openssl dgst -engine gost -sign #{key}", stdin_data: data, binmode: true)
 
-  def sign_file key, cert, file
-    stdout, stderr, status = Open3.capture3("openssl smime -sign -engine gost -gost89  -inkey #{key} -signer #{cert} -in #{file} -outform DER -binary", binmode: true)
-
-    if status.success?
-      return stdout.strip
-    else
-      raise Error.new("sign_file failed: #{stderr}")
+      if status.success?
+        return (Base64.encode64 stdout).strip
+      else
+        raise Error.new("sign failed: #{stderr}")
+      end
     end
+
+
+    def verify_file signature, file
+      stdout, stderr, status = Open3.capture3("openssl smime -verify -engine gost -noverify -inform DER -in #{signature} -content #{file}", binmode: true)
+
+      if status.success?
+        return /successful/ =~ stdout
+      else
+        raise Error.new("verify_file failed: #{stderr}")
+      end
+    end
+
+    def sign_file key, cert, file
+      stdout, stderr, status = Open3.capture3("openssl smime -sign -engine gost -gost89  -inkey #{key} -signer #{cert} -in #{file} -outform DER -binary", binmode: true)
+
+      if status.success?
+        return stdout.strip
+      else
+        raise Error.new("sign_file failed: #{stderr}")
+      end
+    end
+    
+    def extract_certs rawxml
+      xml = Nokogiri.XML(rawxml)
+      xml.remove_namespaces!
+      certs = []
+      certs += xml.search('X509Certificate')
+      certs +=  xml.search('BinarySecurityToken')
+      return certs.map() do |cert|
+        Ssl::Certificate.new(Base64.decode64(cert.text))
+      end.uniq{|c| c.serial.to_i}
+    end
+    
   end
-  
-  def extract_certs rawxml
-    xml = Nokogiri.XML(rawxml)
-    xml.remove_namespaces!
-    certs = []
-    certs += xml.search('X509Certificate')
-    certs +=  xml.search('BinarySecurityToken')
-    return certs.map() do |cert|
-      OpenSSL::X509::Certificate.new(Base64.decode64(cert.text))
-    end.uniq{|c| c.serial.to_i}
+
+  class Certificate < OpenSSL::X509::Certificate
+    
+      def initialize args = nil
+          super(args)
+          
+          begin
+              stdout, stderr, status = Open3.capture3('openssl x509 -noout -text -certopt no_pubkey -certopt no_sigdump -nameopt oneline,-esc_msb', stdin_data: self.to_s, binmode: true)
+              stdout = stdout.split("\n")
+          rescue => e
+              puts "Exception: #{e}"
+              return
+          end
+          
+          begin
+              self.issuer = OpenSSL::X509::Name.parse repack_name(stdout.grep(/Issuer/).first, 'Issuer')
+          rescue => e
+              puts "Exception: #{e}"
+          end
+          
+          begin
+              self.subject = OpenSSL::X509::Name.parse repack_name(stdout.grep(/Subject/).first, 'Subject')
+          rescue => e
+              puts "Exception: #{e}"
+          end
+          
+          begin
+            not_after = Time.parse(not_after).utc
+            not_before = Time.parse(not_before).utc
+          rescue
+          end
+      end
+      
+      def subjectx
+          self.subject.to_a.inject({}) do |ret, v| ret[v[0]] = v[1].force_encoding('utf-8'); ret; end 
+      end
+      
+      def issuerx
+          self.issuer.to_a.inject({}) do |ret, v| ret[v[0]] = v[1].force_encoding('utf-8'); ret; end
+      end
+
+      def extensionsx
+          return self.extensions.inject({}) do |ret, ext|
+            ret[ext.oid] = ext.to_h
+            
+            priv = ret['privateKeyUsagePeriod']
+            if priv
+                not_before, not_after = priv['value'].split(',')
+                
+                not_before = not_before.sub('Not Before:', '').strip
+                not_after = not_after.sub('Not After:', '').strip
+                
+                priv['Not Before'] = Time.parse(not_before).utc
+                priv['Not After'] = Time.parse(not_after).utc
+                ret['privateKeyUsagePeriod'] = priv
+            end
+            
+            ret
+          end
+      end
+      
+      def repack_name name, type
+          return name.sub("#{type}:", '').strip.gsub(" = ", "=").gsub(", ", "/")
+      end
+      
   end
-  
+
 end
-
-
 
 
 
